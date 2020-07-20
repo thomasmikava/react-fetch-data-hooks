e01a98f853c224ff877f8bdcb718ecb6dc412172
temuri <koddddddi@gmail.com>
2020-07-21 00:18:47 +0400

    enhance createResourceLoadingHook

--- src/index.ts
+++ src/index.ts
@@ -25,10 +25,12 @@ export const createResourceLoadingHook = <DefaultKey extends string | null>({
 	resourceKey,
 	dependenciesInfoHook: useDependenciesInfo = defaultDependenciesInfo,
 	defaultIsIdentificationKnownFn,
+	defaultForcefullyFetchFn = defaultProvidedForcefullyFetchFn,
 }: {
 	resourceKey: DefaultKey;
 	dependenciesInfoHook?: DepsInfoHook;
 	defaultIsIdentificationKnownFn?: (deps: readonly any[]) => boolean;
+	defaultForcefullyFetchFn?: (resource: any) => boolean;
 }) => {
 	type DataExt = DefaultKey extends string ? any : Record<any, any>;
 	type FinalData<Data> = DefaultKey extends string
@@ -56,23 +58,36 @@ export const createResourceLoadingHook = <DefaultKey extends string | null>({
 			},
 			dependencies: Deps
 		): ResourceLoading<FinalData<Data>, () => void, any>;
+		<Data extends DataExt, Deps extends readonly any[]>(
+			resource: Data | null | undefined,
+			fetch: (
+				args: CurrentState<Deps>
+			) => undefined | Promise<unknown>,
+			isIdentificationKnown: boolean,
+			dependencies: Deps
+		): ResourceLoading<FinalData<Data>, () => void, any>;
+		<Data extends DataExt, Deps extends readonly any[]>(
+			resource: Data | null | undefined,
+			fetch: (
+				args: CurrentState<Deps>
+			) => undefined | Promise<unknown>,
+			dependencies: Deps
+		): ResourceLoading<FinalData<Data>, () => void, any>;
 	};
 
 	const useResourceLoading: UseResourceLoading = (<
 		DOC extends DataExt,
 		Deps extends readonly any[]
-	>(
-		{
-			resource,
-			fetch,
-			forcefullyFetch,
-		}: {
-			resource: DOC | null | undefined;
-			fetch: (args: CurrentState<Deps>) => undefined | Promise<unknown>;
-			forcefullyFetch?: boolean;
-		},
-		...args: any[]
+	>(...args: any[]
 	) => {
+		const {
+			resource,
+			forcefullyFetch,
+			fetch,
+			isIdentificationKnown: passedIsIdentificationKnown,
+			dependencies
+		} = decipherUseResourceLoadingArgs<DOC, Deps>(args);
+
 		const [error, setError] = useState<any>(null);
 		const forceUpdate = useForceUpdate();
 
@@ -87,13 +102,21 @@ export const createResourceLoadingHook = <DefaultKey extends string | null>({
 
 		const isMountedRef = useMountInfo();
 
-		const dependencies = args[args.length - 1];
 		const depsInfo = useDependenciesInfo(...dependencies);
 
-		if (depsInfo.getStableVersion() === 0 && !forcefullyFetch) {
-			if (resource !== null && resource !== undefined) {
-				depsInfo.setAsStable();
-			}
+		const finalForcefullyFetch =
+			forcefullyFetch === undefined
+				? defaultForcefullyFetchFn
+					? defaultForcefullyFetchFn(resource)
+					: false
+				: forcefullyFetch;
+		const forcefullyFetchVersion = usePivotVersion(
+			finalForcefullyFetch,
+			true
+		);
+
+		if (depsInfo.getStableVersion() === 0 && forcefullyFetchVersion === 0) {
+			depsInfo.setAsStable();
 		}
 
 		const getResource = () => {
@@ -138,18 +161,22 @@ export const createResourceLoadingHook = <DefaultKey extends string | null>({
 		const currentVersion = depsInfo.getVersion();
 
 		const isIdentificationKnown =
-			args.length === 1
+			passedIsIdentificationKnown === undefined
 				? defaultIsIdentificationKnownFn
 					? defaultIsIdentificationKnownFn(dependencies)
 					: true
-				: !!args[0];
+				: passedIsIdentificationKnown;
 
 		useEffect(() => {
 			if (!isIdentificationKnown) return;
 			if (!depsInfo.isStableVersion()) {
 				getResourceRef.current();
 			}
-		}, [depsInfo, currentVersion, isIdentificationKnown, forcefullyFetch]);
+		}, [
+			currentVersion,
+			isIdentificationKnown,
+			forcefullyFetchVersion,
+		]);
 
 		const fetched = depsInfo.isStableVersion();
 
@@ -175,24 +202,28 @@ export const createResourceLoadingHook = <DefaultKey extends string | null>({
 	return useResourceLoading;
 };
 
-export const createFetchHook = <DefaultKey extends string | null>({
+export const createFetchHook = <DefaultKey extends string | null, SetResourceKey extends string | undefined = undefined>({
 	resourceKey,
 	dependenciesInfoHook = defaultDependenciesInfo,
 	defaultIsIdentificationKnownFn,
+	dangerouslySetResourceKey: setResourceKey
 }: {
 	resourceKey: DefaultKey;
 	dependenciesInfoHook?: DepsInfoHook;
 	defaultIsIdentificationKnownFn?: (deps: readonly any[]) => boolean;
+	dangerouslySetResourceKey?: SetResourceKey
 }) => {
 	const useResourceLoading = createResourceLoadingHook({
-		resourceKey,
+		resourceKey: setResourceKey ? null : resourceKey,
 		dependenciesInfoHook,
 		defaultIsIdentificationKnownFn,
+		defaultForcefullyFetchFn: () => true,
 	});
 	type DataExt = DefaultKey extends string ? any : Record<any, any>;
+	type Additional<Data> = SetResourceKey extends string ? { [key in SetResourceKey]: Dispatch<SetStateAction<Data>> } : {};
 	type FinalData<Data> = DefaultKey extends string
-		? { [key in DefaultKey]: Data }
-		: Data;
+		? ({ [key in DefaultKey]: Data } & Additional<Data>)
+		: (Data & Additional<Data>);
 
 	type UseFetch = {
 		<Data extends DataExt, Deps extends readonly any[]>(
@@ -206,7 +237,7 @@ export const createFetchHook = <DefaultKey extends string | null>({
 		): ResourceLoading<FinalData<Data>, () => void, any>;
 	};
 	const useFetch: UseFetch = (arg: any, ...restArgs: any[]): any => {
-		const [fetchedData, setFetchedData] = useState<any>(null);
+		const [rawResource, setRawResource] = useState<any>(undefined);
 		let fetchResource: any;
 		let rest: any = undefined;
 		if (typeof arg === "function") {
@@ -215,17 +246,22 @@ export const createFetchHook = <DefaultKey extends string | null>({
 			({ fetchResource, ...rest } = arg);
 		}
 
+		const resource = useMemo(() => {
+			if (!setResourceKey) return rawResource;
+			if (resourceKey === null) return { [setResourceKey as string]: setRawResource, ...rawResource};
+			return { [setResourceKey as string]: setRawResource, [resourceKey as string]: rawResource };
+		}, [rawResource]);
+
 		return useResourceLoading(
 			{
-				resource: fetchedData,
+				resource,
 				fetch: (...args) => {
-					return fetchResource(...args).then(resource => {
+					return fetchResource(...args).then(res => {
 						if (!args[0].shouldBeCancelled()) {
-							setFetchedData(resource);
+							setRawResource(res);
 						}
 					});
 				},
-				resourceKey: resourceKey as string,
 				...rest,
 			},
 			...(restArgs as [boolean, any[]])
@@ -248,3 +284,43 @@ const useForceUpdate = () => {
 	const [, forceUpdate] = useReducer(x => x + 1, 0);
 	return forceUpdate as () => void;
 };
+
+const usePivotVersion = <T>(value: T, pivotValue: T): number => {
+	const version = useRef(0);
+	if (value === pivotValue) {
+		version.current++;
+	}
+	return version.current;
+};
+
+const defaultProvidedForcefullyFetchFn = (resource: any): boolean => {
+	return resource === null || resource === undefined;
+};
+
+type SetStateAction<S> = S | ((prevState: S) => S);
+type Dispatch<A> = (value: A) => void;
+
+
+const decipherUseResourceLoadingArgs = <DOC, Deps>(args: any[]) => {
+	let resource: DOC | null | undefined;
+	let forcefullyFetch: boolean | undefined = undefined;
+	let fetch: (args: CurrentState<Deps>) => undefined | Promise<unknown>;
+	let isIdentificationKnown: boolean | undefined = undefined;
+
+	if (typeof args[1] === "function") {
+		resource = args[0];
+		fetch = args[1];
+		if (args.length === 4) {
+			isIdentificationKnown = !!args[3];
+		}
+	} else {
+		resource = args[0].resource;
+		fetch = args[0].fetch;
+		forcefullyFetch = args[0].forcefullyFetch;
+		if (args.length === 3) {
+			isIdentificationKnown = !!args[2];
+		}
+	}
+	const dependencies = args[args.length - 1];
+	return { resource, dependencies, fetch, forcefullyFetch, isIdentificationKnown }
+}
